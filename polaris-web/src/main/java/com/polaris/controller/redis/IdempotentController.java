a26cc51c854da2e6589eab058731a514d40d785a
蒋冠峰 <xianggua.jgf@raycloud.com>
2020-04-29 10:21:22 +0800

    reids 幂等测试

---
+++ polaris-web/src/main/java/com/polaris/controller/redis/IdempotentController.java
@@ -0,0 +1,26 @@
+package com.polaris.controller.redis;
+
+import com.polaris.common.re.BaseResponse;
+import com.polaris.common.redis.TokenService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("idempotent")
+public class IdempotentController {
+    @Resource
+    private TokenService tokenService;
+
+    @RequestMapping("/token")
+    public BaseResponse getToken() {
+        return BaseResponse.buildSuccessResponse("获取token成功", tokenService.createToken());
+    }
+
+    @RequestMapping("/test")
+    public BaseResponse test() {
+        System.out.println("校验幂等性:" + System.currentTimeMillis());
+        return BaseResponse.buildSuccessResponse("接口调用成功,reids幂等成功", "");
+    }
+}
