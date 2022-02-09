e8d7aeffa272baa01c307e93cf76c174c1eb833d
jiangguanfeng <wb-jgf834886@alibaba-inc.com>
2022-02-09 16:19:32 +0800

    pom 引用优化修改 redis 测试demo

--- polaris-common/src/main/java/com/polaris/common/redis/RedisValueServiceImpl.java
+++ polaris-common/src/main/java/com/polaris/common/redis/RedisValueServiceImpl.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.polaris.common.redis;
 
 import org.springframework.stereotype.Service;
@@ -28,3 +29,35 @@ public class RedisValueServiceImpl<K, V> extends AbstractRedisService {
         redisTemplate.opsForValue().increment(k, data);
     }
 }
+=======
+package com.polaris.common.redis;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisValueServiceImpl<K, V> extends AbstractRedisService {
+    public void set(K k, V v) {
+        redisTemplate.opsForValue().set(k, v);
+    }
+    public void hSet(K k,V v){
+        redisTemplate.opsForHash().hasKey(k,v);
+    }
+    public void set(K k, V v, Long time, TimeUnit unit) {
+        redisTemplate.opsForValue().set(k, v, time, unit);
+    }
+
+    public V get(K k) {
+        return (V) redisTemplate.opsForValue().get(k);
+    }
+
+    public void delete(K k) {
+        redisTemplate.opsForValue().set(k, null);
+    }
+
+    public void inc(K k, Long data) {
+        redisTemplate.opsForValue().increment(k, data);
+    }
+}
+>>>>>>> 561c9e6 (pom 引用优化修改)
