2d6485857a7bbf399be9c6c439b1f0897547b68e
jiangguanfeng <wb-jgf834886@alibaba-inc.com>
2022-02-09 17:24:37 +0800

    接入swagger

--- polaris-web/src/main/java/com/polaris/controller/user/UserController.java
+++ polaris-web/src/main/java/com/polaris/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package com.polaris.controller.user;
 
+import com.polaris.common.entity.User;
 import com.polaris.common.re.BaseResponse;
 import com.polaris.common.re.request.UserRequest;
 import com.polaris.controller.BaseController;
 import com.polaris.service.user.UserService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ import java.io.UnsupportedEncodingException;
  */
 @RestController
 @RequestMapping("/user")
+@Api(tags = "用户登陆注册")
 public class UserController extends BaseController {
 
     @Autowired
@@ -25,6 +28,10 @@ public class UserController extends BaseController {
 
     @RequestMapping("login")
     @ResponseBody
+    //swagger返回值注解
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "0 成功,其它为错误,返回格式：{code:0,data[{}]},data中的属性参照下方Model", response = User.class) })
+    @ApiOperation(httpMethod = "post", value = "登陆接口")//swagger 当前接口注解
     public BaseResponse Login(UserRequest userRequest) throws UnsupportedEncodingException {
         userService.login(getResponse(), userRequest);
         return BaseResponse.buildSuccessResponse("", "");
