f05db7f944da11d54a9635190da6651fc1df336d
蒋冠峰 <wb-jgf834886@alibaba-inc.com>
2022-07-27 17:26:34 +0800

    配置lettcue+迟化链接技术

--- polaris-web/src/main/java/com/polaris/configuration/RedisConfiguration.java
+++ polaris-web/src/main/java/com/polaris/configuration/RedisConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+public class RedisConfiguration {
     @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
