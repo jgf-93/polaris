60afcdf96d8d83d32c9957fa529ace16f810c30c
jgf <1186967953@qq.com>
2022-08-01 23:33:46 +0800

    github迁移gitee

--- polaris-web/src/main/java/com/polaris/Application.java
+++ polaris-web/src/main/java/com/polaris/Application.java
@@ -28,4 +28,5 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
     }
+
 }
