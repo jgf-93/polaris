9f3b092bcc8d6fbce6db3c99e55075192463c022
jgf <1186967953@qq.com>
2022-08-06 19:00:21 +0800

    springboot 集成dubbo+zookeeper

---
+++ polaris-provider/src/test/java/com/polaris/DubboProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
