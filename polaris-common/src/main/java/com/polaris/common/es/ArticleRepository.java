738ae0b796e46250c36c9f2188e738b435b6b155
jgf <1186967953@qq.com>
2022-08-17 22:24:01 +0800

    springboot 集成es  支持模糊查询以及查询

--- polaris-common/src/main/java/com/polaris/common/es/ArticleRepository.java
+++ polaris-common/src/main/java/com/polaris/common/es/ArticleRepository.java
@@ -2,5 +2,11 @@ package com.polaris.common.es;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends ElasticsearchRepository<ArticleES, Long> {
+
+    ArticleES findByTitle(String title);
+
+    List<ArticleES> findByTitleOrContent(String title,String content);
 }
--- polaris-web/src/test/java/com/polaris/EsTest.java
+++ polaris-web/src/test/java/com/polaris/EsTest.java
@@ -3,9 +3,13 @@ package com.polaris;
 import com.alibaba.fastjson.JSON;
 import com.polaris.common.es.ArticleES;
 import com.polaris.common.es.ArticleRepository;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class EsTest extends Test {
@@ -21,13 +25,27 @@ public class EsTest extends Test {
     @org.junit.Test
     public void saveArticle() throws Exception {
         ArticleES articleES = new ArticleES();
-        articleES.setId(UUID.randomUUID().getLeastSignificantBits());
+        articleES.setId(System.currentTimeMillis());
         articleES.setImg("test.jpg");
-        articleES.setTitle("测试文章");
-        articleES.setContent("本次测试文章含有奖励");
+        articleES.setTitle("小陈文章经典");
+        articleES.setContent("小陈文章包含火影忍者的内容");
         articleES.setCreateTime(new Date());
         articleES.setUpdateTime(new Date());
-        articleES=articleRepository.save(articleES);
+        articleES = articleRepository.save(articleES);
         System.out.println(JSON.toJSONString(articleES));
+    }
+
+    /**
+     * es 关键词模糊查询
+     */
+    @org.junit.Test
+    public void searchArticleByKeyWord() throws Exception {
+        //ArticleES articleES = articleRepository.findByTitle("小蒋");
+//        List<ArticleES> articleESList = articleRepository.findByTitleOrContent("小蒋", "忙");
+//        System.out.println(JSON.toJSONString(articleESList));
+        QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                .must(QueryBuilders.matchPhraseQuery("title.keyword", "小蒋文章"));
+        Iterable<ArticleES> aList = articleRepository.search(queryBuilder);
+        System.out.println(JSON.toJSONString(aList));
     }
 }
